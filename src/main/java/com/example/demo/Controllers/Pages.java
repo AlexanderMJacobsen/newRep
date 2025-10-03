0e6732475ae79a52b9759571ab8a05fa25274fa1
Alexander <alja0004@stud.ek.dk>
2025-10-03 11:26:06 +0200

    using controller for pages class instead of rest

--- src/main/java/com/example/demo/Controllers/Pages.java
+++ src/main/java/com/example/demo/Controllers/Pages.java
@@ -1,14 +1,17 @@
 package com.example.demo.Controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Pages {
 
-    @GetMapping("/")
-    public String welcome() {
-        return "Welcome to the countries page!!!";
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("messeage", "welcome");
+        return "home/index";
     }
-
 }
